dde0d99f5a7ed8c5d2dff6ba59a844a052420598
matbakutis <31048002+matbakutis@users.noreply.github.com>
2024-01-12 11:16:57 -0600

    Initialized Springboot Project

---
+++ src/main/java/com/practice/springboot/practicewebapp/PracticewebappApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.practicewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticewebappApplication.class, args);
+	}
+
+}
